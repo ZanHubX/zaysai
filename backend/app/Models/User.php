7dd8fac744694af7e1dbfdf3a6483d211d0f740b
Kaung Zan Thaw (Wang) <kaungzanthaw27904@gmail.com>
2026-09-14 14:08:50 +0630

    feat: add store model and database structure

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,11 +9,16 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    public function store(): HasOne
+    {
+        return $this->hasOne(Store::class);
+    }
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
